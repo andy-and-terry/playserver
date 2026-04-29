7e5646d5e2f6afb5157b0a8e85504cf77a8a2d99
andy-and-terry <yucheng.lee29@gmail.com>
2026-04-29 17:00:12 +0800

    Update content for AI and Games sections
    Set span elements for AI and Games sections to 'Unavailable'.

---
+++ site/index.js
@@ -0,0 +1,18 @@
+    //NOTE:
+    //For links use this: <a href="https://abc.com" target="_blank">Click here</a>
+
+    /*==============================*/
+    
+    /*==========AI==========*/
+    // Get the span element by its ID
+    var spanElement = document.getElementById("ai");
+
+    // Change the content to include an anchor tag 
+    spanElement.innerHTML = 'Unavalible';
+
+    /*==========GAMES==========*/
+    // Get the span element by its ID
+    var spanElement = document.getElementById("games");
+
+    // Change the content to include an anchor tag 
+    spanElement.innerHTML = 'Unavalible';
